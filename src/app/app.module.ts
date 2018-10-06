a44b5754e1b17872e9f33aa385fa9a2323948140
Vladimir-Ioan <36692556+vladimirioan82@users.noreply.github.com>
2018-10-06 23:30:28 +0300

    start learning...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NbThemeModule, NbStepperModule, NbLayoutModule } from '@nebular/theme';
+
 import { AppComponent } from './app.component';
-import { MyModuleModule } from './my-module/my-module.module';
-import { MyCompComponent } from './my-module/my-comp/my-comp.component';
+import { NebularControllersModule } from './modules/nebular-controllers/nebular-controllers.module';
+import { NebularStepperComponent } from './components/nebular-stepper/nebular-stepper.component';
+import { NebularFlipCardComponent } from './components/nebular-flip-card/nebular-flip-card.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule,
-    MyModuleModule,
-    RouterModule.forRoot([{ path: '', component: MyCompComponent }]),
+    FormsModule,    
+    NebularControllersModule,
+    RouterModule.forRoot([{ path: '', component: NebularStepperComponent }]),
     NbLayoutModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbStepperModule
     ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NebularFlipCardComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
